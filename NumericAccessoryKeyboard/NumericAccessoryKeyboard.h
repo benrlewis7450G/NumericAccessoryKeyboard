16f8e95ed3ec2c7a88ba8d2702cbc76dff9d1239
Benjamin Lewis <benrlewis@gmail.com>
2022-01-18 12:35:52 -0600

    Initial Commit

---
+++ NumericAccessoryKeyboard/NumericAccessoryKeyboard.h
@@ -0,0 +1,18 @@
+//
+//  NumericAccessoryKeyboard.h
+//  NumericAccessoryKeyboard
+//
+//  Created by Benjamin Lewis on 1/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NumericAccessoryKeyboard.
+FOUNDATION_EXPORT double NumericAccessoryKeyboardVersionNumber;
+
+//! Project version string for NumericAccessoryKeyboard.
+FOUNDATION_EXPORT const unsigned char NumericAccessoryKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumericAccessoryKeyboard/PublicHeader.h>
+
+
